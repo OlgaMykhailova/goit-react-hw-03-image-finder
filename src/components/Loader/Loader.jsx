a40e7382966bf1747b7fbcd1f909c097eec8a35f
OlgaMykhailova <rosebud_z@yahoo.com>
2022-10-20 21:21:16 +0200

    added loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import PacmanLoader from 'react-spinners/PacmanLoader';
+
+export const Loader = () => {
+  return (
+    <div>
+      <PacmanLoader color="#36d7b7" loading speedMultiplier={1} />
+    </div>
+  );
+};
